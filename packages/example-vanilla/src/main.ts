c61ef634d2d529fab631b1f5eb23fcad5ad0570f
Istvan Ujj-Meszaros <istvan@radaptor.com>
2025-10-17 02:07:03 +0200

    fix(angular-example): enable angular devtools in dev mode

--- packages/example-vanilla/src/main.ts
+++ packages/example-vanilla/src/main.ts
@@ -1,8 +1,15 @@
 import 'zone.js';
 import '@angular/compiler';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, enableDebugTools } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import './styles.css';
 import '@touchspin/renderer-vanilla/css';
+import { ApplicationRef, isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
+  .then((appRef: ApplicationRef) => {
+    if (isDevMode()) {
+      enableDebugTools(appRef);
+    }
+  })
+  .catch((err) => console.error(err));
